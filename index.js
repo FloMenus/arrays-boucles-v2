87a1c05ac7f7a8a3fe01e713fb45a5d3ec919412
Florent Menus <florent.mns@gmail.com>
2022-06-22 16:29:42 +0200

    finished ex. 2

---
+++ index.js
@@ -0,0 +1,39 @@
+// 1- Nombre pairs
+
+
+
+// let addition = 0
+
+// for (let i=11; i<47; i++) {
+//     if (i % 2 === 0) {
+//         console.log (i)
+//         addition = addition + i
+//     }
+// }
+// console.log (addition)
+
+
+//  2- Nombre impairs 
+
+
+// let addition = 0
+
+// for (let i=109; i<588; i++) {
+//     if (i % 2 !== 0) {
+//         console.log (i)
+//         addition = addition + i
+//     }
+// }
+// console.log (addition)
+
+
+
+// 3- Puissance et racine carrée
+
+const numbers =[1, 2, 3, 4, 5]
+const squares= []
+const roots= []
+
+for (let i=0; i=numbers.length; i++) {
+    
+}
